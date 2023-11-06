5d9c810e518f75754b34d34158404cb362fd2daa
Slayergirl0428 <149774473+Slayergirl0428@users.noreply.github.com>
2023-11-06 09:55:24 +0200

    I am just starting out

---
+++ codeChallenge2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class codeChallenge2 {
+
+
+   public static void main(String[] args)  {
+      Scanner s = new Scanner(System.in);
+
+   System.out.print("Enter your name and age: ");
+   System.out.println(s.nextLine() +"! you are " + s.nextDouble() + "years old");
+   }
+
+}
